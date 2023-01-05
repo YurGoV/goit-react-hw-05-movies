fdf5805f81f621a7b982cb6013f83dff0e00eedf
YurGoV <yurgov@gmail.com>
2023-01-05 02:09:06 +0200

    avatars

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -30,7 +30,7 @@ export const MoviesList = ({movies}) => {
 
   return (
     <Box sx={{flexGrow: 1}}>
-      <Grid container spacing={{xs: 2, md: 3}} columns={{xs: 4, sm: 8, md: 12}}>
+      <Grid container spacing={{xs: 2, md: 3}} columns={{xs: 4, sm: 8, md: 16}}>
 
         {movies.map(({id, original_title, backdrop_path}) => (
 
