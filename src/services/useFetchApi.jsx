df6357a190c2b604aadda248a2f036d972e5fbb3
YurGoV <yurgov@gmail.com>
2023-01-06 17:12:08 +0200

    udeFetchApiHookInMovieDetails

--- src/services/useFetchApi.jsx
+++ src/services/useFetchApi.jsx
@@ -4,34 +4,38 @@ import axios from "axios";
 const API_KEY = 'fe13ab826a741d40ca015441d0a0f529';
 const BACKEND = 'https://api.themoviedb.org/3/';
 
-export const useFetchApi = (query = null, page = 1) => {
+export const useFetchApi = () => {
 
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const [movieDetails, setMovieDetails] = useState(0)
+
   // console.log('useHook');
 
-  const fetchPopular =  useCallback(  async () => {
+  const fetchPopular =  useCallback(  async (query = null, page = 1, type = 'popular') => {
       try {
-        const response = await axios.get(`${BACKEND}trending/movie/day?api_key=${API_KEY}`);
-        const answer = await response.data.results;
-        setMovies(answer);
-        return answer;
-      } catch (error) {
+        if (type === 'popular' ) {
+          // console.log(type);
+          const response = await axios.get(`${BACKEND}trending/movie/day?api_key=${API_KEY}`);
+          const answer = await response.data.results;
+          setMovies(answer);
+          // return answer;
+        }
+        if (type === 'details' && query ) {//todo: if (query.movieId)
+          // console.log(query);
+          const response = await axios.get(`${BACKEND}movie/${query.movieId}?api_key=${API_KEY}`);
+          const answer = await response.data;
+          setMovieDetails(answer);
+        }
+        } catch (error) {
         // console.log(error.message);
         return error;
       }
     }, []);
-    // await fetch();
 
 
   useEffect(() => {
-    // console.log('useHook2');
-    if (!query && page) {
-      // console.log('useHook3');
       fetchPopular()
-    }
+  }, [fetchPopular]);
 
-  }, [fetchPopular, page, query]);
-
-  return {movies, fetchPopular}
-
+  return {movies, movieDetails, fetchPopular}
 };
